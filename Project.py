458f7e31fb5479c455d1e7c2f61be4a78f1cd663
Zhenghong Huang <alanmax@me.com>
2022-04-05 05:53:56 +0800

    Add multi threading to runner

--- Project.py
+++ Project.py
@@ -1,2 +1,10 @@
+#Manager & Plotter, NOT IMPLEMENTED
 import numpy as np
 import matplotlib as plt
+
+import threading
+
+from algorithm import *
+from runner import runner
+
+
--- src/runner.py
+++ src/runner.py
@@ -1,9 +1,10 @@
-from email import generator
+from multiprocessing import Pool, TimeoutError
+import multiprocessing
 from typing import List, Tuple
-import generator as gen
-import algorithm as algo
+from generator import mapGenerator
+from algorithm import *
 
-def runner(X: int, Y: int, count: int, fnc: object, N = None, P = None, Locales = None) -> List[Tuple[bool, int]]: 
+def runner(X: int, Y: int, fnc: object, N = None, P = None, Locales = None) -> List[Tuple[bool, int]]: 
     params = {
         "X": X,
         "Y": Y,
@@ -13,9 +14,31 @@ def runner(X: int, Y: int, count: int, fnc: object, N = None, P = None, Locales 
     }    
     notNoneParams = {k:v for k, v in params.items() if v is not None}
 
-    resultList = []
-    for i in range(count):
-        resultList.append(fnc(gen.mapGenerator(**notNoneParams)))
+    return fnc(mapGenerator(**notNoneParams))
+    # resultList = []
+    # for i in range(count):
+    #     resultList.append(fnc(mapGenerator(**notNoneParams)))
 
-    return resultList
+    # return resultList
+
+def paralleledRunner(X: int, Y: int, count: int, fnc: object, N = None, P = None, Locales = None) -> List[Tuple[bool, int]]:
+    resultList = []
+    workers = multiprocessing.cpu_count()
+
+    if __name__ == '__main__':
+        with Pool(processes=workers) as pool:
+            multipleResults = [pool.apply_async(runner, (X,Y,fnc,N,P,Locales)) for i in range(count)]
+            try:
+                resultList = [res.get(timeout = 1) for res in multipleResults]
+            except TimeoutError:
+                print("TIMEOUT ON RUNNER")
+        return resultList
+    
+def singleThreadRunner(X: int, Y: int, count: int, fnc: object, N = None, P = None, Locales = None) -> List[Tuple[bool, int]]:
+    return [runner(X,Y,fnc,N,P,Locales) for i in range(count)]
+
+
+
+        
+
     
