5a299010a19cdab3ad44636473b677a1e32ac4d9
Zhenghong Huang <alanmax@me.com>
2022-04-05 15:35:55 +0800

    Updated BFS Algorithm with intended efficiency

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,4 +1,6 @@
+from time import sleep
 from typing import List, Tuple
+import sys
 import numpy as np
 import generator as gen
 import math
@@ -7,20 +9,25 @@ import math
 def EZ_BFS(mapGen: gen.mapGenerator) -> Tuple[bool, int]:
     tileMap = mapGen.getMap()
     tileList = mapGen.getTileLocales()
-    channelList = mapGen.getChannelLocales()
 
     allStep = 0
 
     for i in tileList:
+        # print(tileMap)
+    
         cpyMap = np.copy(tileMap)
         (result, steps) = __EZ_BFS_ALGO(i,cpyMap)
         if result == False:
-            #print(cpyMap)
+            # print(cpyMap)
             return (False, -1)
         else:
             allStep += (len(steps) - 1)
             tileMap[i[0]][i[1]] = 0
-    
+        
+        # for i in range(len(tileMap)):
+        #     sys.stdout.write("\033[F") #back to previous line 
+        #     sys.stdout.write("\033[K") #clear line 
+            
     return (True, allStep)
 
 def __EZ_BFS_ALGO(start: Tuple[int, int], map: np.ndarray) -> Tuple[bool, int]:
@@ -32,12 +39,23 @@ def __EZ_BFS_ALGO(start: Tuple[int, int], map: np.ndarray) -> Tuple[bool, int]:
         curPath.append(curNode)
         if map[curNode[0]][curNode[1]] == -2:
             return (True, curPath)
+        if map[curNode[0]][curNode[1]] == -4:
+            pass
+        else:
+            # DEBUG & DISPLAY MODULE
+            # sleep(0.5)
+            # 
+            # for i in range(len(map)):
+            #     sys.stdout.write("\033[F") #back to previous line 
+            #     sys.stdout.write("\033[K") #clear line 
 
-        map[curNode[0]][curNode[1]] = -4 #-4 as visited
+            map[curNode[0]][curNode[1]] = -4 #-4 as visited
+            # print(map)
+        
 
-        avaliableAdjNodes = __EZ_BFS_GETNEIGHBORS(curNode, map)
-        for node in avaliableAdjNodes:
-            stepQueue.append((node, curPath[:]))
+            avaliableAdjNodes = __EZ_BFS_GETNEIGHBORS(curNode, map)
+            for node in avaliableAdjNodes:
+                stepQueue.append((node, curPath[:]))
     
     return (False, None)
 
@@ -55,3 +73,9 @@ def __EZ_BFS_GETNEIGHBORS(node: Tuple[int, int], map: np.ndarray) -> List[Tuple[
 
     return neighborList
 
+#TILE SORTING USING 
+def A_STAR_SORTING(mapGen: gen.mapGenerator) -> Tuple[bool,int]:
+    pass
+
+# res = EZ_BFS(gen.mapGenerator(30,30,P=0.01))
+# print(res)
